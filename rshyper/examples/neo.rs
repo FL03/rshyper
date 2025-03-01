6b7850fbbafd6220f11b6f373560666efe03f3a3
Joe McCain III <jo3mccain@icloud.com>
2025-03-01 14:56:31 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- rshyper/examples/neo.rs
+++ rshyper/examples/neo.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+
 /*
     Appellation: neo <example>
     Contrib: @FL03
 */
+#[allow(unused_imports)]
 use rshyper::{HyperGraph, Node};
-use utm::{State, Symbol, demonstrate_tonnetz_and_utm};
+use utm::{demonstrate_tonnetz_and_utm, State, Symbol};
 
 fn main() {
     use utm::Symbol::*;
@@ -25,6 +28,12 @@ fn main() {
     };
     println!("Facet data for hyperedge {e1:?}: {:?}", facet_data);
 
+    // let machine = TM::new(Head { state: State::S0, symbol: A }, vec![A, B, C]).with_rules(sample_rulest());
+
+    // for (head, position) in machine {
+    //     println!("Step => head: {head:?}, head_position: {position}");
+    // }
+
     // Demonstrate how this triad serves as a "headspace" of a Wolfram [2, 3] UTM
     let triad = graph
         .get_edge_nodes(e1)
@@ -33,6 +42,62 @@ fn main() {
         .map(|&v| v.weight())
         .collect::<Vec<_>>();
     demonstrate_tonnetz_and_utm(triad);
+}
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Head<Q = State, S = Symbol> {
+    pub state: Q,
+    pub symbol: S,
+}
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct TM {
+    pub head: Head,
+    pub tape: Vec<Symbol>,
+    pub position: usize,
+    pub rules: HashMap<Head, (Head, &'static str)>,
+}
+
+impl TM {
+    pub fn new(head: Head, tape: Vec<Symbol>) -> Self {
+        Self {
+            head,
+            tape,
+            position: 0,
+            rules: HashMap::new(),
+        }
+    }
+
+    pub fn with_rules(self, rules: HashMap<Head, (Head, &'static str)>) -> Self {
+        Self { rules, ..self }
+    }
+
+
+    pub fn tape(&self) -> &[Symbol] {
+        &self.tape
+    }
+
+    pub fn tape_mut(&mut self) -> &mut Vec<Symbol> {
+        &mut self.tape
+    }
+}
+
+impl core::iter::Iterator for TM {
+    type Item = (Head, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let &(head, dir) = self.rules.get(&self.head)?;
+        self.tape[self.position] = head.symbol;
+        self.head = head;
+
+        // Move head (no bounds checking for brevity)
+        match dir {
+            "L" => self.position = self.position.checked_sub(1)?,
+            "R" => self.position += 1,
+            _ => {}
+        }
+
+        Some((head, self.position))
+    }
 }
 
 #[derive(Debug)]
@@ -47,7 +112,8 @@ impl MachineFacet {
     }
 }
 
-mod utm {
+pub mod utm {
+    use super::Head;
     // A basic representation of states for a Wolfram [2, 3] UTM
     #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
     #[cfg_attr(
@@ -75,6 +141,37 @@ mod utm {
         E,
         F,
         G,
+    }
+
+    pub fn sample_rulest() -> std::collections::HashMap<Head, (Head, &'static str)> {
+        use State::*;
+        use Symbol::*;
+        let mut rules = std::collections::HashMap::new();
+        rules.insert(
+            Head { state: S0, symbol: A },
+            (Head { state: S1, symbol: B }, "R"),
+        );
+        rules.insert(
+            Head { state: S0, symbol: B },
+            (Head { state: S0, symbol: C }, "L"),
+        );
+        rules.insert(
+            Head { state: S0, symbol: C },
+            (Head { state: S1, symbol: A }, "R"),
+        );
+        rules.insert(
+            Head { state: S1, symbol: A },
+            (Head { state: S0, symbol: C }, "L"),
+        );
+        rules.insert(
+            Head { state: S1, symbol: B },
+            (Head { state: S1, symbol: A }, "R"),
+        );
+        rules.insert(
+            Head { state: S1, symbol: C },
+            (Head { state: S0, symbol: B }, "L"),
+        );
+        rules
     }
 
     // Example ruleset (transition function) for a Wolfram [2, 3] UTM
