3a1955c1d4c85cf48acbd226fa4a1d9fe1ff5407
FL03 <jo3mccain@icloud.com>
2025-06-01 00:29:12 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ rshyper/src/macros.rs
@@ -0,0 +1,13 @@
+/*
+    appellation: macros <module>
+    authors: @FL03
+*/
+//! this module implements various macros used throughout the rshyper framework
+//! 
+//! 
+#[macro_use]
+pub mod hypergraph;
+#[macro_use]
+pub mod hypernode;
+#[macro_use]
+pub mod seal;
--- rshyper/src/macros/hypergraph.rs
+++ rshyper/src/macros/hypergraph.rs
@@ -2,7 +2,8 @@
     appellation: hypergraph <module>
     authors: @FL03
 */
-
+/// the [`hypergraph`] macro works to aide the in the creation of hypergraphs by allowing
+/// users to define nodes in a more structured way.
 #[cfg(feature = "std")]
 #[macro_export]
 macro_rules! hypergraph {
@@ -16,50 +17,5 @@ macro_rules! hypergraph {
     ) => {
         // insert nodes into the graph
         $crate::hypernode!($graph { $(let $var $(= $w)?);* });
-    };
-}
-/// the [`hypernode`] macro streamlines the process of inserting nodes into a hypergraph.
-///
-/// ## Usage
-///
-/// ```no_run
-/// hypernode! {
-///     $source: {
-///         let $var:ident $(= $w:expr)?;
-///     }
-/// }
-/// ```
-///
-/// ### Basic Usage
-///
-/// ```rust
-/// use rshyper::HashGraph;
-///
-/// let mut graph: HashGraph<usize, usize> = HashGraph::new();
-/// // use the macro to insert nodes into the graph
-/// rshyper::hypernode! {
-///     graph {
-///         let v0;
-///         let v1;
-///         let v2;
-///         let v3 = 1;
-///         let v4 = 2;
-///         let v5 = 3;
-///     }
-///  }
-/// ```
-#[macro_export]
-macro_rules! hypernode {
-    ($src:ident { $(let $var:ident $(= $w:expr)?);* $(;)? }) => {
-        $($crate::hypernode!(@impl $src [$var] $(= $w)?);)*
-    };
-    (@impl $src:ident[$var:ident] $(= $w:expr)?) => {
-        $crate::hypernode!(@new $src[$var] $(= $w)?);
-    };
-    (@new $src:ident[$var:ident] = $w:expr) => {
-        let $var = $src.insert_node($w);
-    };
-    (@new $src:ident[$var:ident]) => {
-        let $var = $src.insert_vertex();
     };
 }
