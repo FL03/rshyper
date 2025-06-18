1eef22cc41fa6ff8c2e873c6649c39dc6c632f3a
FL03 <jo3mccain@icloud.com>
2025-06-18 08:39:35 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- hmap/src/types/aliases.rs
+++ hmap/src/types/aliases.rs
@@ -5,15 +5,15 @@
 #[allow(deprecated, unused_imports)]
 pub use self::impl_deprecated::*;
 
-use hashbrown::DefaultHashBuilder;
 use hashbrown::hash_map::{Entry, HashMap};
 use rshyper::idx::{EdgeId, VertexId};
 use rshyper::{Edge, Node};
 
+
+/// a type alias for a [`HashSet`](rshyper::VertexHashSet)
+pub type VertexSet<Idx = usize, S> = rshyper::VertexHashSet<Idx, S>;
 /// a type alias for a [`Edge`] with [`VertexSet`] as its vertices
 pub type HashEdge<E, K, I, S> = Edge<E, VertexSet<I, S>, K, I>;
-/// a type alias for a [`HashSet`](hashbrown::HashSet) of [`VertexId`]
-pub type VertexSet<Idx = usize, S = DefaultHashBuilder> = hashbrown::HashSet<VertexId<Idx>, S>;
 
 /// a type alias for a [`Entry`] that whose key is a [`VertexId`] and value is a [`Node`]
 pub type NodeEntry<'a, N, I, S> = Entry<'a, VertexId<I>, Node<N, I>, S>;
@@ -21,9 +21,9 @@ pub type NodeEntry<'a, N, I, S> = Entry<'a, VertexId<I>, Node<N, I>, S>;
 pub type EdgeEntry<'a, T, K, I, S> = Entry<'a, EdgeId<I>, HashEdge<T, K, I, S>, S>;
 
 /// a type alias for a [`HashMap`] that maps [`VertexId`] to a [`Node`]
-pub type NodeMap<N, I, S = DefaultHashBuilder> = HashMap<VertexId<I>, Node<N, I>, S>;
+pub type NodeMap<N, I, S> = HashMap<VertexId<I>, Node<N, I>, S>;
 /// a type alias for a [`HashMap`] that maps [`EdgeId`] to a [`HashFacet`]
-pub type EdgeMap<E, K, I, S = DefaultHashBuilder> = HashMap<EdgeId<I>, HashEdge<E, K, I, S>, S>;
+pub type EdgeMap<E, K, I, S> = HashMap<EdgeId<I>, HashEdge<E, K, I, S>, S>;
 
 #[allow(deprecated)]
 #[doc(hidden)]
